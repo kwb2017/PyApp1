e6e918b7482c9dcca71437a90067f4b0c2ab82f8
kwb2017 <kunal.bhattacharya@outlook.com>
2017-09-01 15:04:25 +0530

    classes and methods

--- PyApp1/Person.py
+++ PyApp1/Person.py
@@ -1,6 +1,27 @@
-class Person():
-    
+#no interfaces 
+#no private or protected 
 
-person = Person()
+class Person():
+   def __init__(self, name, age):
+    self.person_name = name
+    self.person_age = age
+   
+   def __str__ (self):
+       return "Name: "+ self.person_name + " Age: "+ str(self.person_age)
+
+
+class Employee(Person):
+    def __init__(self,id,person):
+        self.id = id
+        self.person_age = person.person_age
+        self.person_name = person.person_name
+
+    def __str__(self):
+       return  str(Person(self.person_name,self.person_age)) + " Id:"+str(self.id)
+
+
+person = Person("Kunal",32)
+employee = Employee(105,person)
+print(employee)
 
 
